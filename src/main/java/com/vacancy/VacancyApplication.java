3ee67d8d0a438f1a79d62137fd6507cbe9803d67
Kliodt <vadimkl2004@mail.ru>
2025-09-05 11:44:25 +0300

    initial comit

---
+++ src/main/java/com/vacancy/VacancyApplication.java
@@ -0,0 +1,15 @@
+package com.vacancy;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class VacancyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VacancyApplication.class, args);
+    }
+
+}
